f210eed1228f7d0942f1a3bcdb74cf4a2190d94b
Sovak <Sovak007@gmail.com>
2012-06-17 04:14:38 +0200

    New opcodes :)
    Signed-off-by: Sovak <Sovak007@gmail.com>

--- src/server/game/Handlers/NPCHandler.cpp
+++ src/server/game/Handlers/NPCHandler.cpp
@@ -295,7 +295,6 @@ void WorldSession::HandleTrainerBuySpellOpcode(WorldPacket & recv_data)
     WorldPacket data(SMSG_TRAINER_BUY_SUCCEEDED, 12);
     data << uint64(guid);
     data << uint32(spellId);                                // should be same as in packet from client
-    data << uint32(0);                                      // "Trainer service", 1 = "Not enough money for trainer service <TS>". 0 = "Trainer service <TS> unavailable"
     SendPacket(&data);
 }
 
@@ -496,7 +495,6 @@ void WorldSession::SendBindPoint(Creature* npc)
     WorldPacket data(SMSG_TRAINER_BUY_SUCCEEDED, (8+4));
     data << uint64(npc->GetGUID());
     data << uint32(bindspell);
-    data << uint32(0);                                      // "Trainer service", 1 = "Not enough money for trainer service <TS>". 0 = "Trainer service <TS> unavailable"
     SendPacket(&data);
 
     _player->PlayerTalkClass->SendCloseGossip();
