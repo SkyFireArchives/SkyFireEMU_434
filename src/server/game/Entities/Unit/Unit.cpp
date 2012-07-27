5c96f58d122184acdfd97d7fdb9febe103d561bb
NTX <NTX@zoznam.sk>
2012-07-27 19:50:51 +0200

    Fix MSG_MOVE_SET_PITCH_RATE

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -13019,25 +13019,25 @@ void Unit::SetSpeed(UnitMoveType mtype, float rate, bool forced)
                 break;
             case MOVE_PITCH_RATE:
                 data.Initialize(MSG_MOVE_SET_PITCH_RATE, 1 + 8 + 4 + 4 );
-                data.WriteBit(bytes[7]);
-                data.WriteBit(bytes[5]);
-                data.WriteBit(bytes[6]);
-                data.WriteBit(bytes[2]);
-                data.WriteBit(bytes[4]);
-                data.WriteBit(bytes[0]);
-                data.WriteBit(bytes[3]);
-                data.WriteBit(bytes[1]);
-
+                data.WriteByteMask(bytes[1]);
+                data.WriteByteMask(bytes[2]);
+                data.WriteByteMask(bytes[6]);
+                data.WriteByteMask(bytes[7]);
+                data.WriteByteMask(bytes[0]);
+                data.WriteByteMask(bytes[3]);
+                data.WriteByteMask(bytes[5]);
+                data.WriteByteMask(bytes[4]);
+                data.FlushBits();
+                data << float(GetSpeed(mtype));
+                data.WriteByteSeq(bytes[6]);
+                data.WriteByteSeq(bytes[4]);
                 data.WriteByteSeq(bytes[0]);
                 data << uint32(0);
-                data.WriteByteSeq(bytes[3]);
-                data << float(GetSpeed(mtype));
-                data.WriteByteSeq(bytes[5]);
                 data.WriteByteSeq(bytes[1]);
-                data.WriteByteSeq(bytes[6]);
-                data.WriteByteSeq(bytes[7]);
                 data.WriteByteSeq(bytes[2]);
-                data.WriteByteSeq(bytes[4]);
+                data.WriteByteSeq(bytes[7]);
+                data.WriteByteSeq(bytes[3]);
+                data.WriteByteSeq(bytes[5]);
                 break;
             default:
                 sLog->outError("Unit::SetSpeed: Unsupported move type (%d), data not sent to client.", mtype);
