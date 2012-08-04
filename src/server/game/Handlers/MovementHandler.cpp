6a75e94a3c3451721e9e2b4d17b6213fb95ca863
SkyFire <admin@projectskyfire.org>
2012-08-04 07:52:51 -0500

    Updated HandleSetActiveMoverOpcode structure. Thx Shauren.
    Signed-off-by: SkyFire <admin@projectskyfire.org>

--- src/server/game/Handlers/MovementHandler.cpp
+++ src/server/game/Handlers/MovementHandler.cpp
@@ -479,8 +479,25 @@ void WorldSession::HandleSetActiveMoverOpcode(WorldPacket &recv_data)
 {
     sLog->outDebug(LOG_FILTER_NETWORKIO, "WORLD: Recvd CMSG_SET_ACTIVE_MOVER");
 
-    uint64 guid;
-    recv_data >> guid;
+    ObjectGuid guid;
+
+    guid[7] = recv_data.ReadBit();
+    guid[2] = recv_data.ReadBit();
+    guid[1] = recv_data.ReadBit();
+    guid[0] = recv_data.ReadBit();
+    guid[4] = recv_data.ReadBit();
+    guid[5] = recv_data.ReadBit();
+    guid[6] = recv_data.ReadBit();
+    guid[3] = recv_data.ReadBit();
+
+    recv_data.ReadByteSeq(guid[3]);
+    recv_data.ReadByteSeq(guid[2]);
+    recv_data.ReadByteSeq(guid[4]);
+    recv_data.ReadByteSeq(guid[0]);
+    recv_data.ReadByteSeq(guid[5]);
+    recv_data.ReadByteSeq(guid[1]);
+    recv_data.ReadByteSeq(guid[6]);
+    recv_data.ReadByteSeq(guid[7]);
 
     if (GetPlayer()->IsInWorld())
     {
