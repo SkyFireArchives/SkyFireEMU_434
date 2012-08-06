eae878cec7d01816d48703cbb30580c1ac6171b4
AriDEV <AriDEV@azet.sk>
2012-08-06 12:52:52 +0200

    DB: Removed Non Existing Spells From NPC_TRAINER * Copper Modulator (3926) * Shield Bash (72) * Sense Demons (5500)
    Signed-off-by: AriDEV <AriDEV@azet.sk>

---
+++ sql/updates/AriDEV/2012_08_06_1_npc_trainer_db_errors_4_3_4.sql
@@ -0,0 +1,10 @@
+-- REMOVE: Copper Modulator From NPC_TRAINER (48 DB ERRORS FIXED)
+DELETE FROM npc_trainer WHERE spell=3926;
+
+-- REMOVE: Shield Bash From NPC_TRAINER (50 DB ERRORS FIXED)
+DELETE FROM npc_trainer WHERE spell=72;
+
+-- REMOVE: Sense Demons From NPC_TRAINER (6 DB ERRORS FIXED)
+DELETE FROM npc_trainer WHERE spell=5500;
+
+#FIXED BY ARIDEV
